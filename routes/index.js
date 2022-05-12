b30a63ee7f628563c902225cf62edc604de8a31f
Shivam Soni <vermashivam217@gmail.com>
2022-05-12 13:32:33 +0530

    created login route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { registerController } from '../controllers';
+import { registerController, loginController } from '../controllers';
 
 
 router.post('/register', registerController.register);
+router.post('/login', loginController.login)
 
 
 export default router;
