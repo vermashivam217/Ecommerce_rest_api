308b7b5d39cfc6c79cb89b3a92087a8fc627afbd
Shivam Soni <vermashivam217@gmail.com>
2022-06-16 18:59:23 +0530

    Refresh token route added

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { registerController, loginController, userController } from '../controllers';
+import { registerController, loginController, userController, refreshController } from '../controllers';
 import auth from '../middlewares/auth'
 
 
 router.post('/register', registerController.register);
 router.post('/login', loginController.login)
 router.get('/me', auth, userController.me)
+router.post('/refresh', auth, refreshController.refresh)
 
 
 export default router;
