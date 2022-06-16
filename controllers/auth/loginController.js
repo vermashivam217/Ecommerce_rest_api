5dcde5a5f787b0ba6599f57b6becca0d0537b492
Shivam Soni <vermashivam217@gmail.com>
2022-06-16 19:36:28 +0530

    logout added

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -41,6 +41,23 @@ const loginController = {
         } catch (error) {
             return next(error)
         }
+    },
+    async logout(req, res, next) {
+        // Validation
+        const refreshSchema = Joi.object({
+            refresh_token: Joi.string().required(),
+        });
+        const { error } = refreshSchema.validate(req.body);
+
+        if (error) {
+            return next(error);
+        }
+        try {
+            await RefreshToken.deleteOne({ token: req.body.refresh_token })
+        } catch (error) {
+            return next(new Error('Something went wrong in the database'))
+        }
+        res.json({ status: 1 })
     }
 }
 
